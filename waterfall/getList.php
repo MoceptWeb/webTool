e1fb933b8e0d9be01cd5e32e1f76290229a838c2
mocept <dzm1846907072@gmail.com>
2016-09-11 22:48:57 +0800

    waterfall

---
+++ waterfall/getList.php
@@ -0,0 +1,40 @@
+<?php
+header("Content-Type:application/json;charset=utf-8");
+echo('{"d": [
+{"img": "http://dummyimage.com/640x3:4", "title": "json:图片1"},
+{"img": "http://dummyimage.com/640x2:1", "title": "json:图片2"},
+{"img": "http://dummyimage.com/480x4:3", "title": "json:图片3"},
+{"img": "http://dummyimage.com/640x3:2", "title": "json:图片4"},
+{"img": "http://dummyimage.com/640x3:2", "title": "json:图片5"},
+{"img": "http://dummyimage.com/640x3:2", "title": "json:图片6"},
+{"img": "http://dummyimage.com/640x3:2", "title": "json:图片7"},
+ {"img": "http://dummyimage.com/640x3:2", "title": "json:图片8"},
+{"img": "http://dummyimage.com/640x1:2", "title": "json:图片9"},
+{"img": "http://dummyimage.com/640x3:2", "title": "json:图片10"},
+{"img": "http://dummyimage.com/640x3:1", "title": "json:图片11"},
+{"img": "http://dummyimage.com/480x3:2", "title": "json:图片12"},
+{"img": "http://dummyimage.com/640x3:2", "title": "json:图片13"},
+{"img": "http://dummyimage.com/200x3:2", "title": "json:图片14"},
+{"img": "http://dummyimage.com/640x3:2", "title": "json:图片15"},
+{"img": "http://dummyimage.com/100x3:2", "title": "json:图片16"},
+{"img": "http://dummyimage.com/640x3:1", "title": "json:图片17"},
+{"img": "http://dummyimage.com/640x3:4", "title": "json:图片18"},
+{"img": "http://dummyimage.com/640x3:2", "title": "json:图片19"},
+{"img": "http://dummyimage.com/640x3:1", "title": "json:图片20"}
+]}');
+/*
+ {"img": "http://dummyimage.com/640x3:2", "title": "图片8"},
+{"img": "http://dummyimage.com/640x1:2", "title": "图片9"},
+{"img": "http://dummyimage.com/640x3:2", "title": "图片10"},
+{"img": "http://dummyimage.com/640x3:1", "title": "图片11"},
+{"img": "http://dummyimage.com/480x3:2", "title": "图片12"},
+{"img": "http://dummyimage.com/640x3:2", "title": "图片13"},
+{"img": "http://dummyimage.com/200x3:2", "title": "图片14"},
+{"img": "http://dummyimage.com/640x3:2", "title": "图片15"},
+{"img": "http://dummyimage.com/100x3:2", "title": "图片16"},
+{"img": "http://dummyimage.com/640x3:1", "title": "图片17"},
+{"img": "hhttp://dummyimage.com/640x3:4", "title": "图片18"},
+{"img": "http://dummyimage.com/640x3:2", "title": "图片19"},
+{"img": "http://dummyimage.com/640x3:1", "title": "图片20"}
+ */
+?>

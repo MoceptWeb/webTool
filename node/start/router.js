949154a70d56fbca5305300d9cf7fc013a12dcc7
mocept <dzm1846907072@gmail.com>
2017-02-21 12:36:58 +0800

    upload img

--- node/start/router.js
+++ node/start/router.js
@@ -1,14 +1,13 @@
-function route(handler, pathname, response, postData) {
-    console.log("About to router a request for " + pathname);
-    if (typeof handler[pathname] === 'function') {
-        return handler[pathname](response, postData);
-    } else {
-        console.log("No request handler found for " + pathname);
-        return "404 NOT found";
-        response.writeHead(404, {"Content-Type": "text/plain"});
-        response.write("404 Not found");
-        response.end();
-    }
+function route(handle, pathname, response, request) {
+  console.log("About to route a request for " + pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](response, request);
+  } else {
+    console.log("No request handler found for " + pathname);
+    response.writeHead(404, {"Content-Type": "text/html"});
+    response.write("404 Not found");
+    response.end();
+  }
 }
 
 exports.route = route;

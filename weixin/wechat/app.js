de50a2eac571f5ae438b8481be5389c9278cb727
mocept <dzm1846907072@gmail.com>
2017-05-04 20:35:04 +0800

    【weixin】:art:

--- weixin/wechat/app.js
+++ weixin/wechat/app.js
@@ -2,30 +2,12 @@
 
 var Koa = require('koa')
 var sha1 = require('sha1')
+var wechat = require('./wechat/g')
 var config = require('./config/config')
 
 var app = new Koa()
 
-app.use(function *(next) {
-  console.log(this.query);
+app.use(wechat(config.wechat))
 
-  var token = config.wechat.token
-  var signature = this.query.signature
-  var nonce = this.query.nonce
-  var timestamp = this.query.timestamp
-  var echostr = this.query.echostr
-  console.log(config)
-
-  var str = [token, timestamp, nonce].sort().join('')
-  var sha = sha1(str)
-
-  if( sha === signature) {
-    this.body = echostr + ''
-  } else {
-    this.body = 'wrong'
-  }
-
-});
-
-app.listen(4444);
-console.log('server start');
+app.listen(4444)
+console.log('server start')
